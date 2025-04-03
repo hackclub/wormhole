5afca952d0ba999b56bace08e130f1acbf2f9cdd
Christopher Walker <15720465+polytroper@users.noreply.github.com>
2025-04-03 12:57:14 -0600

    Fix HTTPS in deployment issue

--- vite.config.js
+++ vite.config.js
@@ -4,19 +4,22 @@ import fs from "fs";
 import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react(), tailwindcss()],
-  server: {
-    https: {
-      key: fs.readFileSync("localhost+2-key.pem"),
-      cert: fs.readFileSync("localhost+2.pem"),
-    },
-    port: 5173,
-    proxy: {
-      "/api": {
-        target: "http://localhost:3000",
-        changeOrigin: true,
-      },
-    },
-  },
-});
+  server:
+    command === "serve"
+      ? {
+          https: {
+            key: fs.readFileSync("localhost+2-key.pem"),
+            cert: fs.readFileSync("localhost+2.pem"),
+          },
+          port: 5173,
+          proxy: {
+            "/api": {
+              target: "http://localhost:3000",
+              changeOrigin: true,
+            },
+          },
+        }
+      : undefined,
+}));
